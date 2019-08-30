ad60939fc767e7afe509ae62fba734a4ad775e7e
zionHuang <hjszion@gmail.com>
2019-08-30 01:08:59 -0700

    very first node-commit

--- code/01-node-test.js
+++ code/01-node-test.js
@@ -2,7 +2,18 @@ console.log('Hi, node js, my first demo!');
 var a = 9;
 console.log(++a);
 
-var a = 10; //a 的作用域仅仅在.js文件内部
-setInterval(function(){
-    console.log(new Date());
-}, 1000);
+// var a = 10; //a 的作用域仅仅在.js文件内部
+// setInterval(function(){
+//     console.log(new Date());
+// }, 1000);
+
+// console.error(111111);
+
+console.dir(Math);
+console.log(Object.prototype);
+console.time('123');
+var t = 0;
+for(var i=0; i<1000; i++){
+    t += 1;
+}
+console.timeEnd('123');
