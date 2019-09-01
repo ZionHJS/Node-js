64235a4d4474995e7b6bcc0897dae8712ef554a7
zionHuang <hjszion@gmail.com>
2019-08-31 23:37:15 -0700

    added http-module

---
+++ code/23-http-module.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+//当发生了 request 请求事件的时候，自动调用回调函数
+http.createServer(function(req, res){
+    //req 监听数据
+    // req.on('data',function(trunk){
+    // });
+    console.log(req.header);
+    console.log(req.url);
+    console.log(req.method);
+
+    res.write('hello 123');
+    res.end();
+
+    //伪代码:
+    // fs.readFile(path, ()=>{
+    //     fs.readFile(path,()=>{
+    //         fs.readFile(path,()=>{
+    //             fs.readFile(path,()=>{
+    //                 //...
+    //             })
+    //         })
+    //     })
+    // })
+    //解决方法 connect:express 
+}).listen(58990,()=>{
+    console.log('server listening success! http://127.0.0.1:58990');
+});
