791428d2a8dc8c1e98b8ceb1ab813ac7db456c67
zionHuang <hjszion@gmail.com>
2019-09-01 15:56:42 -0700

    added art-template

--- express-CRUD/app.js
+++ express-CRUD/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const art_express = require('express-art-template');
 
+const app = express();   //创建一个app对象
+
 //设置art的模版引擎
 app.engine('art', art_express);
-
-const app = express();   //创建一个app对象
 
 //用express提供的静态目录的中间件
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,15 +14,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('./user/list', (req,res)=>{
     // res.send('abc');
     // res.end();
+
     //res对象的render方法 第一个参数是模版的文件名 第二个参数是给模版的数据
-    
-    res.render('userlist.art',{
-        title:'hello!!!'
+    //express引擎会自动到view文件夹下搜索文件 如果是子文件夹则需要指明是哪个文件夹
+    res.render('./users/userlist2.art',{
+        title:'hello!!!',
+        users:[{name:'david',id:"10011"},
+        {name:'mike',id:"10012"}]
     });
 });
 
-console.log(11223344555);
+console.log(11223344555666);
 
 app.listen(59999, () => {
     console.log('http://127.0.0.1:59999');
 });
+
