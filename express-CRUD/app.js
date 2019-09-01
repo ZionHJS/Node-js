146a397aa8ced428502c48eb0963dccb93728ef1
zionHuang <hjszion@gmail.com>
2019-09-01 15:27:32 -0700

    added art-template

--- express-CRUD/app.js
+++ express-CRUD/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const art_express = require('express-art-template');
+
+//设置art的模版引擎
+app.engine('art', art_express);
 
 const app = express();   //创建一个app对象
 
@@ -8,11 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //express动态请求
 app.get('./user/list', (req,res)=>{
-    res.send('abc');
-    res.end();
+    // res.send('abc');
+    // res.end();
+    //res对象的render方法 第一个参数是模版的文件名 第二个参数是给模版的数据
+    
+    res.render('userlist.art',{
+        title:'hello!!!'
+    });
 });
 
-console.log(11223344);
+console.log(11223344555);
 
 app.listen(59999, () => {
     console.log('http://127.0.0.1:59999');
--- express-CRUD/readme.md
+++ express-CRUD/readme.md
@@ -1,3 +1,15 @@
 #nodemon 来进行自动重启应用
+nodemon包如果安装错误 需要sudo su 进入管理权限然后进行安装
 npm i -g nodemon
-使用:nodemon app.js
+使用:nodemon app.js 来代替node app.js
+
+#art-template模版使用
+第一步:安装art-template包
+npm install --save art-template
+npm install --save express-art-template
+
+第二步:项目中设置express的应用art-template模版引擎
+
+
+
+
