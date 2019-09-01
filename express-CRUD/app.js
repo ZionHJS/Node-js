a836e439355fda0163ef39c5e5dcd88575fa324d
zionHuang <hjszion@gmail.com>
2019-09-01 12:44:49 -0700

    added express

---
+++ express-CRUD/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();   //创建一个app对象
+
+//用express提供的静态目录的中间件
+app.use(express.static(path.join(__dirname, 'public')));
+
+//express动态请求
+app.get('./user/list', (req,res)=>{
+    res.send('abc');
+    res.end();
+});
+
+app.listen(59999, () => {
+    console.log('http://127.0.0.1:59999');
+});;
