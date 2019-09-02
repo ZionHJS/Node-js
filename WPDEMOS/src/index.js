c970ded17a7680289deda2aa786808e81c990509
zionHuang <hjszion@gmail.com>
2019-09-02 16:58:22 -0700

    added webpack-css

--- WPDEMOS/src/index.js
+++ WPDEMOS/src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import './style/index.css';//loader => css-loader   module, style-loader
+
 
 function createDomElement(){
     let dom = document.createElement('div');
     dom.innerHTML = _.join(['google.com', 'well', 'internship'], '');
+    dom.classList.add('box');
     return dom;
 }
 
