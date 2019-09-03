d739c7dce109c6f835174eae0a1947be6c3466e8
zionHuang <hjszion@gmail.com>
2019-09-02 21:39:48 -0700

    added autoprefixer

--- WPDEMOS/webpack.config.js
+++ WPDEMOS/webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    entry:'./src/index.js',
-    mode:'development',
-    output:{
-        filename:'main.js',
-        path:path.resolve(__dirname, 'dist')
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:['style-loader','css-loader']
+                test: /\.(sc|c|sa)ss$/,
+                use: ['style-loader',
+                 {loader: 'css-loader', options: { sourceMap: true }}, 
+                 {loader:'postcss-loader', options:{
+                     ident:'postcss',
+                     plugins:(loader) => {
+                         require('autoprefixer')({browsers:['>0.15% in CN']})
+                     }
+                 }}
+                 {loader:'sass-loader', options:{sourceMap:true}}]
             }
         ]
     }
